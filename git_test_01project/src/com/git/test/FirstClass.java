f439730a2e451f978a1fc57b0d6f24c350154f83
James <zhanshixiang1997@163.com>
2019-08-02 16:03:59 +0800

    把git_test_01project 本地工程添加到Git

---
+++ git_test_01project/src/com/git/test/FirstClass.java
@@ -0,0 +1,12 @@
+package com.git.test;
+
+/**
+ * @author James
+ * @create 2019-08-02 15:55
+ */
+public class FirstClass {
+
+    public static void main(String[] args){
+        System.out.println("在idea下使用git进行版本控制.....");
+    }
+}
